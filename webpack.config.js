670b17582cd1803c897fac9ee34a182143aa695c
kihyeoon <bangion93@gmail.com>
2022-09-26 22:46:24 +0900

    ✨ feat: dev-server, mini-css, 클린웹팩 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: "./src/script.js",
@@ -15,7 +17,8 @@ module.exports = {
         // 배열 마지막 요소부터 오른쪽에서 왼쪽 순으로 적용
         // 먼저 css-loader가 적용되고, styled-loader가 적용되어야 한다.
         // 순서 주의!
-        use: ["style-loader", "css-loader"],
+        //* use: ["style-loader", "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
         // loader가 node_modules 안의 있는 내용도 처리하기 때문에
         // node_modules는 제외해야 합니다
         exclude: /node_modules/,
@@ -26,5 +29,15 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "src", "index.html"),
     }),
+    new MiniCssExtractPlugin({
+      filename: "common.css",
+    }),
+    new CleanWebpackPlugin(),
   ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "docs"),
+    },
+    port: 8080,
+  },
 };
